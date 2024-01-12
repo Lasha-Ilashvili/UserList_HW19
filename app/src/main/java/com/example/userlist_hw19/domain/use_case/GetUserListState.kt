d53de5b1c4a06ac70c84e6b9974e52bf54732769
Lasha-Ilashvili <ilashvililasha@gmail.com>
2024-01-13 03:21:33 +0400

    experimental commit

--- app/src/main/java/com/example/userlist_hw19/domain/use_case/GetUserListState.kt
+++ app/src/main/java/com/example/userlist_hw19/domain/use_case/GetUserListState.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.flow.Flow
 class GetUserListState(private val userListRepository: UserListRepository) {
 
     suspend operator fun invoke(): Flow<UserListResult<List<GetUser>>> {
-        /*Use mapper*/
         return userListRepository.getUserList()
     }
 }
